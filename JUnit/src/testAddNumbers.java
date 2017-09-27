c572086c450f4d71a26e37b69a3205d59e4a93f4
kashish52 <30345155+kashish52@users.noreply.github.com>
2017-09-27 23:35:58 +0530

    initial commit

--- JUnit/src/testAddNumbers.java
+++ JUnit/src/testAddNumbers.java
@@ -3,6 +3,9 @@ import static org.junit.Assert.*;
 public class testAddNumbers {
     @org.junit.Test
     public void addNumber() throws Exception {
+        jUnitTesting obj = new jUnitTesting();
+        int results=obj.addNumber(3,5);
+        assertEquals(8,results);
     }
 
 }
--- JUnit/src/testAddStrings.java
+++ JUnit/src/testAddStrings.java
@@ -6,9 +6,7 @@ import static org.junit.Assert.*;
 public class testAddStrings {
     @Test
     public void addStrings() throws Exception {
-        jUnitTesting obj = new jUnitTesting();
-        int results=obj.addNumber(3,5);
-        assertEquals(8,results);
+
     }
 
 }
